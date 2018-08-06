a16843a17ef704852202926f9c5f406f9cf887db
Michael Zhang <failed.down@gmail.com>
2018-08-05 23:16:20 -0700

    set up vue boilerplate stuff

---
+++ web/http/routes.js
@@ -0,0 +1,10 @@
+import PageIndex from "../pages/Index";
+
+export default [
+	{
+		path: "/",
+		name: "index",
+		component: PageIndex,
+		title: "Home",
+	},
+];
---
+++ web/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<navbar></navbar>
+	</div>
+</template>
+
+<script>
+	import Navbar from "~/components/Navbar";
+
+	export default {
+		components: { Navbar },
+	};
+</script>
