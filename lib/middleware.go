e5c78c36ee36a68fcbaadccdc6862c23b473f8e8
Michael Zhang <failed.down@gmail.com>
2018-08-06 01:30:18 -0700

    cors

--- lib/middleware.go
+++ lib/middleware.go
@@ -57,6 +57,7 @@ func (app *App) authMiddleware(next http.Handler) http.Handler {
 func (app *App) jsonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
 	})
 }
