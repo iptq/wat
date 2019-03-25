fcc6cf9db7cc8975400f732ffd5205395de878a7
Michael Zhang <iptq@protonmail.com>
2019-03-25 08:12:34 -0500

    login screen

--- app/pages/Login.vue
+++ app/pages/Login.vue
@@ -1,20 +1,80 @@
 <template>
-	<div>
-		<md-layout>
-		<md-card>
-		<md-card-header>
-		<div class="md-title">Card without hover effect</div>
-		</md-card-header>
+	<div class="centered-container">
+		<md-content class="md-elevation-3">
+            <div class="form">
+                <md-field>
+                    <label>Username</label>
+                    <md-input v-model="username" autofocus></md-input>
+                </md-field>
 
-		<md-card-content>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-		</md-card-content>
+                <md-field md-has-password>
+                    <label>Password</label>
+                    <md-input v-model="password" type="password"></md-input>
+                </md-field>
+            </div>
 
-		<md-card-actions>
-		<md-button>Action</md-button>
-		<md-button>Action</md-button>
-		</md-card-actions>
-		</md-card>
-	</md-layout>
+            <div class="actions md-layout">
+                <!-- <a href="/resetpassword">Reset password</a> -->
+                <md-button class="md-raised md-primary" @click="login">Log in</md-button>
+            </div>
+
+            <div class="loading-overlay" v-if="loading">
+                <md-progress-spinner md-mode="indeterminate" :md-stroke="2"></md-progress-spinner>
+            </div>
+    	</md-content>
 	</div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                loading: false,
+                username: "",
+                password: "",
+            };
+        },
+        methods: {
+            login() {
+                this.loading = true;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .centered-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        margin-top: 30px;
+
+
+        .form {
+            margin-bottom: 60px;
+        }
+
+        .md-content {
+            z-index: 1;
+            padding: 40px;
+            width: 100%;
+            max-width: 450px;
+            position: relative;
+        }
+
+        .loading-overlay {
+            z-index: 10;
+            top: 0;
+            left: 0;
+            right: 0;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background: rgba(255, 255, 255, 0.9);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+</style>
